cb6db542bc2de1f7ae355c312f2d3b4903743ec8
astanakhvc <astana.kahveci@gmail.com>
2014-10-03 02:14:43 -0500

    created 3 ui elements
    app next goal: enter weight, see it in tableview.

--- toothless/ViewController.swift
+++ toothless/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tblWeightHistory: UITableView!
+    @IBOutlet weak var txtWeight: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
