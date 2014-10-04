015ed817fb58b292eaf14d13cc9bf890583cee1f
astana <astanakahveci@gmail.com>
2014-10-03 19:43:53 -0500

    working on getting some dummy data in tableview
    learned:
    - _ in method args
    - making new instance of object
    - uitableviewdatasource cellforrowatindexpath the code inside is not
    right, you gotta comment it, start typing, look at the autocomplete

--- toothless/ViewController.swift
+++ toothless/ViewController.swift
@@ -9,19 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var tblWeightHistory: UITableView!
     @IBOutlet weak var txtWeight: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("starting trouble")
+        let weightHistoryTVC = weightHistoryVC()
+        weightHistoryTVC.tableView = self.tblWeightHistory
+        self.tblWeightHistory.delegate = weightHistoryTVC
+        self.tblWeightHistory.dataSource = weightHistoryTVC
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+ 
 }
 
